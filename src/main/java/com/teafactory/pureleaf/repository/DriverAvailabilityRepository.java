ebca0fa6a8ab4468cb4928d69118ffc0102f7a6a
MaheshK0901 <maheshkumara0901@gmail.com>
2025-07-20 14:41:45 +0530

    Add driver availability APIs

---
+++ src/main/java/com/teafactory/pureleaf/repository/DriverAvailabilityRepository.java
@@ -0,0 +1,13 @@
+package com.teafactory.pureleaf.repository;
+
+import com.teafactory.pureleaf.entity.DriverAvailability;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DriverAvailabilityRepository extends JpaRepository<DriverAvailability, Long> {
+    List<DriverAvailability> findByDriver_DriverId(Long driverId);
+}
+
