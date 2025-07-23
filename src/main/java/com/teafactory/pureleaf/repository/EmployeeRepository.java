4ecc7dd00b9d87ad751d858793d7b0788e39e080
dasunT22 <dasuntharinda2535@gmail.com>
2025-07-23 09:30:01 +0530

    Remove unwanted file

--- src/main/java/com/teafactory/pureleaf/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.teafactory.pureleaf.repository;
-
-import com.teafactory.pureleaf.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long>  {
-}
