ec03d854a16533e8ff5763d9d4b87f9082e301cb
dasunT22 <dasuntharinda2535@gmail.com>
2025-07-23 09:27:30 +0530

    create add managers part

---
+++ src/main/java/com/teafactory/pureleaf/service/UserServiceImpl.java
@@ -0,0 +1,69 @@
+package com.teafactory.pureleaf.service;
+
+import com.teafactory.pureleaf.dto.UserDTO;
+import com.teafactory.pureleaf.entity.User;
+import com.teafactory.pureleaf.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDTO createUser(UserDTO userDTO) {
+        if (userRepository.existsByEmail(userDTO.getEmail())){
+            throw new RuntimeException("Email is already in use: " + userDTO.getEmail());
+        }
+        User user = new User();
+        BeanUtils.copyProperties(userDTO,user);
+        user.setCreatedAt(LocalDateTime.now());
+        user.setUpdatedAt(LocalDateTime.now());
+        User savedUser = userRepository.save(user);
+        UserDTO savedDTO = new UserDTO();
+        BeanUtils.copyProperties(savedUser,savedDTO);
+        return savedDTO;
+    }
+
+    @Override
+    public List<UserDTO> getAllUsers() {
+        return userRepository.findAll().stream().map(user ->{
+            UserDTO dto = new UserDTO();
+            BeanUtils.copyProperties(user,dto);
+            return dto;
+        }).collect(Collectors.toList());
+    }
+
+    @Override
+    public UserDTO getUserById(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found with id: " + id));
+        UserDTO dto = new UserDTO();
+        BeanUtils.copyProperties(user,dto);
+        return dto;
+    }
+
+    @Override
+    public UserDTO updateUser(Long id, UserDTO userDTO) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found with id: " + id));
+        BeanUtils.copyProperties(userDTO, user, "id", "createdAt");
+        user.setUpdatedAt(LocalDateTime.now());
+        User updatedUser = userRepository.save(user);
+        UserDTO dto = new UserDTO();
+        BeanUtils.copyProperties(updatedUser,dto);
+        return dto;
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
