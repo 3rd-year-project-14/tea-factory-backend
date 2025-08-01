872bc34ec5fdfacbfebf0d8f8075aa4ed3706045
PasinduMadushanDias <pmdwijegunasinghe@gmail.com>
2025-08-01 08:03:06 +0530

    create update method for tea rate

--- src/main/java/com/teafactory/pureleaf/payment/repository/TeaRateRepository.java
+++ src/main/java/com/teafactory/pureleaf/payment/repository/TeaRateRepository.java
@@ -2,6 +2,8 @@ package com.teafactory.pureleaf.payment.repository;
 
 import com.teafactory.pureleaf.payment.entity.TeaRate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface TeaRateRepository extends JpaRepository<TeaRate, Long> {
+
 }
