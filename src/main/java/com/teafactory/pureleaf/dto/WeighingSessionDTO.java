438edc98869f8d769e3d5035adc5f281ab927fa9
MaheshK0901 <maheshkumara0901@gmail.com>
2025-08-13 16:28:01 +0530

    Create tea weighing process

--- src/main/java/com/teafactory/pureleaf/dto/WeighingSessionDTO.java
+++ src/main/java/com/teafactory/pureleaf/dto/WeighingSessionDTO.java
@@ -6,6 +6,7 @@ import java.time.LocalDate;
 @Data
 public class WeighingSessionDTO {
     private Long tripId;
+    private Long sessionId;
     private LocalDate sessionDate;
     private Long userId;
     private String userName;
