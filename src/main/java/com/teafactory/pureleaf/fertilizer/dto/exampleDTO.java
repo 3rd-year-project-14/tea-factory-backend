1293d8c40a43bbd0b2f26a1756cc21d9a45a1ad9
PasinduMadushanDias <pmdwijegunasinghe@gmail.com>
2025-10-17 20:06:00 +0530

    update properties

---
+++ src/main/java/com/teafactory/pureleaf/fertilizer/dto/exampleDTO.java
@@ -0,0 +1,13 @@
+package com.teafactory.pureleaf.fertilizer.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class exampleDTO {
+    private Long id;
+    private String name;
+}
