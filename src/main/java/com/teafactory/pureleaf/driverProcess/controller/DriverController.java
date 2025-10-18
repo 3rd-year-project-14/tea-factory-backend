4da05ede51f6122b058919634b14d7db098b20d4
dasunT22 <dasuntharinda2535@gmail.com>
2025-10-18 15:27:28 +0530

    add drivers

--- src/main/java/com/teafactory/pureleaf/driverProcess/controller/DriverController.java
+++ src/main/java/com/teafactory/pureleaf/driverProcess/controller/DriverController.java
@@ -1,12 +1,11 @@
 package com.teafactory.pureleaf.driverProcess.controller;
 
 import com.teafactory.pureleaf.driverProcess.dto.DriverDTO;
+import com.teafactory.pureleaf.driverProcess.dto.DriverRegistrationDTO;
 import com.teafactory.pureleaf.driverProcess.service.DriverService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -28,6 +27,12 @@ public class DriverController {
     public ResponseEntity<DriverDTO> createDriver(@RequestBody DriverDTO driverDTO) {
         DriverDTO createdDriver = driverService.createDriver(driverDTO);
         return ResponseEntity.ok(createdDriver);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<DriverDTO> registerDriver(@RequestBody DriverRegistrationDTO registrationDTO) {
+        DriverDTO created = driverService.registerDriver(registrationDTO);
+        return ResponseEntity.ok(created);
     }
 
     @GetMapping("/user/{userId}")
